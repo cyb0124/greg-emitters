f88cc1a7bb5b90efbf312b7eaafb6750993d962f
Yibo Cao <cyb@ieee.org>
2024-07-04 11:24:58 -0400

    pack natives for different archs as separate files

--- forge-1.20.1/src/main/java/cyb0124/greg_emitters/Mod.java
+++ forge-1.20.1/src/main/java/cyb0124/greg_emitters/Mod.java
@@ -1,8 +1,5 @@
 package cyb0124.greg_emitters;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.sun.jna.*;
 import com.sun.jna.platform.linux.Mman;
 import com.sun.jna.platform.unix.LibCUtil;
@@ -10,36 +7,36 @@ import com.sun.jna.platform.win32.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.nio.ByteBuffer;
 import java.util.Map;
 
 @net.minecraftforge.fml.common.Mod("greg_emitters")
 public class Mod {
     public Mod() {
-        JsonObject data;
-        try (InputStream is = Mod.class.getResourceAsStream("/native.json")) {
-            data = new Gson().fromJson(new String(is.readAllBytes(), StandardCharsets.UTF_8), JsonElement.class).getAsJsonObject();
+        String path;
+        if (Platform.ARCH.equals("x86-64")) path = "/x64.bin";
+        else if (Platform.ARCH.equals("aarch64")) path = "/aarch64.bin";
+        else throw new UnsupportedOperationException("Unsupported architecture: " + Platform.ARCH);
+        byte[] blob;
+        try (InputStream is = Mod.class.getResourceAsStream(path)) {
+            blob = is.readAllBytes();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String prefix;
-        if (Platform.ARCH.equals("x86-64")) prefix = "x64-";
-        else if (Platform.ARCH.equals("aarch64")) prefix = "aarch64-";
-        else throw new UnsupportedOperationException("Unsupported architecture: " + Platform.ARCH);
-        byte[] blob = Base64.getDecoder().decode(data.get(prefix + "b").getAsString());
+        ByteBuffer buf = ByteBuffer.wrap(blob, blob.length - 4, 4);
+        int len = buf.getInt();
         Pointer mem = Platform.isWindows()
-                ? Kernel32.INSTANCE.VirtualAllocEx(WinBase.INVALID_HANDLE_VALUE, null, new BaseTSD.SIZE_T(blob.length), WinNT.MEM_COMMIT, WinNT.PAGE_READWRITE)
-                : LibCUtil.mmap(null, blob.length, Mman.PROT_READ | Mman.PROT_WRITE, Mman.MAP_PRIVATE | Mman.MAP_ANON, -1, 0);
-        mem.write(0, blob, 0, blob.length);
-        for (JsonElement item : data.get(prefix + "r").getAsJsonArray()) {
-            long r = item.getAsLong();
-            mem.setLong((int) (r >> 32), Pointer.nativeValue(mem) + (int) r);
+                ? Kernel32.INSTANCE.VirtualAllocEx(WinBase.INVALID_HANDLE_VALUE, null, new BaseTSD.SIZE_T(len), WinNT.MEM_COMMIT, WinNT.PAGE_READWRITE)
+                : LibCUtil.mmap(null, len, Mman.PROT_READ | Mman.PROT_WRITE, Mman.MAP_PRIVATE | Mman.MAP_ANON, -1, 0);
+        mem.write(0, blob, 0, len);
+        buf.position(len);
+        while (buf.remaining() > 12) {
+            mem.setLong(buf.getInt(), Pointer.nativeValue(mem) + buf.getInt());
         }
-        long exec_len = data.get(prefix + "x").getAsLong() * 4096;
+        long exec_len = (long) buf.getInt() * 4096;
         if (Platform.isWindows()) {
             NativeLibrary.getInstance("kernel32").getFunction("VirtualProtect").invoke(new Object[]{mem, exec_len, WinNT.PAGE_EXECUTE_READ, new WinDef.DWORDByReference()});
-            mem = mem.share(data.get(prefix + "w").getAsLong());
+            mem = mem.share(buf.getInt());
         } else {
             NativeLibrary.getInstance("c").getFunction("mprotect").invoke(new Object[]{mem, exec_len, Mman.PROT_READ | Mman.PROT_EXEC});
         }
--- pack-native.py
+++ pack-native.py
@@ -1,12 +1,7 @@
-import base64
-import json
 import os
 
-OUT_PATHS = ['forge-1.20.1/src/generated/resources/native.json']
-x64_map = open('native/target/x64.txt', 'r').readlines()
-x64_bin = open('native/target/x64-custom/release/native', 'rb').read()
-aarch64_map = open('native/target/aarch64.txt', 'r').readlines()
-aarch64_bin = open('native/target/aarch64-custom/release/native', 'rb').read()
+OUT_DIRS = ['forge-1.20.1/src/generated/resources']
+ARCHS = ['x64', 'aarch64']
 
 def find_symbol(linker_map, name):
   lines = list(filter(lambda x: name in x, linker_map))
@@ -19,28 +14,27 @@ def find_symbol(linker_map, name):
   size = int(line[0:line.find(' ')], base=16)
   return addr, size
 
-result = dict()
-for map, bin, prefix in [(x64_map, x64_bin, "x64"), (aarch64_map, aarch64_bin, "aarch64")]:
+for dir in OUT_DIRS: os.makedirs(os.path.dirname(dir), exist_ok=True)
+
+for arch in ARCHS:
+  map = open(f'native/target/{arch}.txt', 'r').readlines()
+  bin = open(f'native/target/{arch}-custom/release/native', 'rb').read()
   reloc, reloc_size = find_symbol(map, ' out_reloc')
-  result[prefix + '-b'] = base64.b64encode(bin[:reloc]).decode()
-  exec_size, _ = find_symbol(map, ' out_rw')
-  assert exec_size % 4096 == 0
-  result[prefix + '-x'] = exec_size // 4096
-  relocs = []
+  out = bytearray(bin[:reloc])
   for i in range(reloc, reloc + reloc_size, 24):
     cur = bin[i:i+24]
     offset = int.from_bytes(cur[0:8], 'little')
     kind = int.from_bytes(cur[8:16], 'little')
     assert kind == 8 or kind == 1027
     addend = int.from_bytes(cur[16:], 'little')
-    relocs.append((offset << 32) | addend)
-  result[prefix + '-r'] = relocs
-
-win, _ = find_symbol(x64_map, ' entry_win64')
-result['x64-w'] = win
-result['aarch64-w'] = 0
-
-for path in OUT_PATHS:
-  os.makedirs(os.path.dirname(path), exist_ok=True)
-  with open(path, 'w') as f:
-    f.write(json.dumps(result))
+    out += offset.to_bytes(4, 'big')
+    out += addend.to_bytes(4, 'big')
+  exec_size, _ = find_symbol(map, ' out_rw')
+  assert exec_size % 4096 == 0
+  out += (exec_size // 4096).to_bytes(4, 'big')
+  win = find_symbol(map, ' entry_win64')[0] if arch == 'x64' else 0
+  out += win.to_bytes(4, 'big')
+  out += reloc.to_bytes(4, 'big')
+  for dir in OUT_DIRS:
+    with open(f'{dir}/{arch}.bin', 'wb') as f:
+      f.write(out)
